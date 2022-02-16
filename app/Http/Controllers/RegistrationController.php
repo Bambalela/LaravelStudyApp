331d98bc377d372fde266a8e64eea75aa4987b96
vlad <yarysh.vlad@gmail.com>
2022-02-16 10:35:01 +0200

    registration & publishing users to database is finished

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\MyUser;
 use App\Http\Requests\RegistrationRequest;
 
 class RegistrationController extends Controller {
     public function submit(RegistrationRequest $request){
-        dd($request);
+        //dd($request);
+        $user = new MyUser();
+        $user->name = $request->input('name');
+        $user->surname = $request->input('surname');
+        $user->email = $request->input('email');
+        $user->password = md5($request->input('password'));
+
+        $user->save();
+        return redirect()->route('home')->with('success', "Registration completed with success");
+
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,6 @@
 
 @section('title')Головна@endsection
 @section('body-content')
+    @include('inc.message')
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ipsum nam repellat? Alias assumenda illum itaque necessitatibus non officia omnis quo reiciendis rem reprehenderit sunt temporibus, tenetur unde, ut voluptas.</p>
 @endsection
